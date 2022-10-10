5cfd415615a52e074bf1462c60ca14b1195d7ddd
nicolasromer <nicolasromer@gmail.com>
2022-10-10 12:20:53 +0200

    master: extract todoform

--- src/todolist/ToDoList.tsx
+++ src/todolist/ToDoList.tsx
@@ -1,8 +1,7 @@
 import { useState, ReactElement, useReducer } from 'react';
-import ToDo from './ToDo.tsx'
+import ToDo from './ToDo'
+import TodoInput from './TodoInput'
 import './ToDoList.css';
-
-
 
 interface Todo {
 	uid?: string;
@@ -13,26 +12,10 @@ const getTodoListItems = (todos: Array<Todo>) => todos.map(({uid, text}) => (
 	<ToDo key={uid} uid={uid} text={text}/>
 ));
 
-const initialState = () => ({text: ""});
-
-function todoFormReducer(state, action) {
-	console.log(state,action)
-	switch (action.type) {
-		case 'text': 
-			return {...state, text: action.value};
-		case 'submit':
-			return initialState();
-    	default:
-      		throw new Error();
-	}
-}
-
 function ToDoList() {
 
 	// todo: persist
 	const [todos, setTodos] = useState<Array<Todo>>([]);
-	
-	const [formState, dispatch] = useReducer(todoFormReducer, initialState());
 	
 	const addTodo = (todoText: string) => setTodos([...todos, {
 		// move to server side
@@ -42,16 +25,8 @@ function ToDoList() {
 
 	return (
 		<div className="todo-list">
-			<input
-				className="border-black border-2"
-				value={formState.text}
-				onChange={e => dispatch({type: 'text', value: e.target.value })}
-				type="text"
-			/>
-			<button 
-				disabled={!formState.text}
-				onClick={e => {addTodo(formState.text); dispatch({type: 'submit'})} }
-			>Create</button>
+			
+			<TodoInput submit={(formState:{text:string}) => addTodo(formState.text)}/>
 
 			<ol>
 				{getTodoListItems(todos)}
---
+++ src/todolist/TodoInput.tsx
@@ -0,0 +1,42 @@
+ import {useReducer, ReactElement} from 'react';
+
+const initialState = () => ({text: ""});
+
+function todoFormReducer(state, action) {
+	console.log(state,action)
+	switch (action.type) {
+		case 'text':
+			return {...state, text: action.value};
+		case 'submit':
+			return initialState();
+    	default:
+      		throw new Error();
+	}
+}
+
+function TodoInput({
+ 	submit
+ }): ReactElement {
+
+	const [formState, dispatch] = useReducer(todoFormReducer, initialState());
+
+ 	return (
+ 		<div id="new-todo">
+			<input
+				className="border-black border-2"
+				value={formState.text}
+				onChange={e => dispatch({type: 'text', value: e.target.value })}
+				type="text"
+			/>
+			<button 
+				disabled={!formState.text}
+				onClick={e => {
+					submit(formState);
+					dispatch({type: 'submit'})
+				}}
+			>Create</button>
+		</div>
+	);
+ }
+
+ export default TodoInput;
